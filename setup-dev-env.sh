537befdb82b27d6d9635fcba8078eb24ddff903a
Hiren Tailor <Hiren.tailor@baesystems.com>
2018-06-01 14:30:05 +0100

    Updated config and entry script

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -12,4 +12,17 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
+# Setting Up
+me="$(whoami)"
+sudo mkdir -p /home/$me/Documents/Ansible
+sudo echo $me > /home/$me/Documents/Ansible/username.txt
+sudo mkdir -p /opt/Ansible
+sudo mv /home/$me/Documents/Ansible/username.txt /opt/Ansible
+sudo rm /home/$me/Documents/Ansible/username.txt
+
+#Ansible magic
 sudo ansible-pull -U https://github.com/jaredpepper2/Development-Setup-via-Ansible
+
+#Clean up
+sudo rm //opt/Ansible/username.txt
+sudo rm /home/$me/Documents/Ansible/username.txt
